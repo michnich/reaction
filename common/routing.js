94446c99c6eb4241026ba080507ac4b1c669b4eb
ehughestaylor <ehughestaylor@hotmail.com>
2016-02-21 17:29:55 -0500

    style tips landing image and style tips page.  Added Terms of use page.

--- common/routing.js
+++ common/routing.js
@@ -4,7 +4,7 @@
  * ReactionCore common/routing.js contains the core routes.
  */
 
-let staticPages = ["about", "team", "faqs", "terms", "privacy" ];
+let staticPages = ["about", "team", "faqs", "privacy" ];
 
 /**
  * app router mapping
@@ -23,6 +23,7 @@ Router.map(function route() {
 Router.map(function route() {
   _.extend(Router.routes.index.options, {
     // Just an example. You can put any Iron Router options here.
-    template: 'landingPro'
+    template: 'landingPro',
+
   });
 });
