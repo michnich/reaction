c8809fc67f1860bdc088cd8eb4a97421a9e90026
ehughestaylor <ehughestaylor@hotmail.com>
2016-04-08 17:47:26 -0400

    hid sandals and jewelery and changed email to isaacs for when people have listed products

--- client/templates/product/add-product.js
+++ client/templates/product/add-product.js
@@ -51,7 +51,7 @@ Template.addProduct.events({
 
       // // Send email to huntrs
       Meteor.call('sendEmail', {
-        to: 'ehughestaylor@codedbykids.com',
+        to: 'isaac@onehunted.com',
         from: 'no-reply@huntrs.com',
         subject: userEmail + ' has added a new product, please review',
         text: 'Mailgun is totally awesome for sending emails!',
