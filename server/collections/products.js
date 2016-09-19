ae00abb3f2c781a309b2cdd4c70af054769c6672
codedbykids2 <smobleyii@gmail.com>
2016-09-18 22:37:50 -0400

    added productsByUser, returns published products based on passed meteor user id

--- server/collections/products.js
+++ server/collections/products.js
@@ -2,3 +2,9 @@
 Meteor.publish('userProducts', function(){
   return userProducts.find();
 });
+
+//pass the function the user id
+//returns all published products posted by that user
+Meteor.publish('productsByUser', function(userId) {
+	return ReactionCore.Collections.Products.find({vendor: userId});
+});
