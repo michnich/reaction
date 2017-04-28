e875333b82686ab32246e521c17dc84d2c020a73
Mick <michnich07@gmail.com>
2017-04-27 23:25:17 -0400

    added sign up modal and styled

--- client/templates/product/product.js
+++ client/templates/product/product.js
@@ -1,26 +1,14 @@
 Template.shop.rendered = function() {
-	if (!Meteor.user()) {
-	    var showSignUp = window.setTimeout(function() {
-			swal({
-				  title: '<span style="color: white">Join the Hunt</span>',
-				  text: "It's free!",
-				  background: url('resources/popup.png'),
-				  showCancelButton: true,
-				  confirmButtonColor: '#3085d6',
-				  cancelButtonColor: '#d33',
-				  confirmButtonText: 'Sign Me Up',
-				  cancelButtonText: 'Not Today',
-				  confirmButtonClass: 'btn btn-danger',
-				  cancelButtonClass: 'btn btn-default',
-				  buttonsStyling: false
-				}).then(function () {
-					Router.go('/signUp');
-				}, function (dismiss) {
-				  // dismiss can be 'cancel', 'overlay',
-				  // 'close', and 'timer'
-				  if (dismiss === 'cancel') {
-				  }
-				});
-		}, 20000);
+	if (Meteor.user().emails.length === 0) {
+		window.setTimeout(function(){
+	  		Modal.show('popup');
+		}, 15000);
 	}
 };
+
+Template.popup.events({
+	'click #signUpPopup': function() {
+		Router.go('sell');
+		Modal.hide('popup');
+	}
+});
