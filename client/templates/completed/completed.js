493db6146d87d3f12b5dfb76eb11362197ac3103
Mick <michnich07@gmail.com>
2017-05-07 19:14:26 -0400

    purchased completed email with details

--- client/templates/completed/completed.js
+++ client/templates/completed/completed.js
@@ -1,39 +1,37 @@
 Template.cartCompleted.onCreated(function() {
-  console.log('cartCompleted created');
-  console.log(Router.current().params._id);
-  console.log(Meteor.userId());
-
   Meteor.subscribe("completedOrder", Meteor.userId(), Router.current().params._id, function(result, error) {
-    console.log('callback');
-    console.log(ReactionCore.Collections.Orders.findOne({}));
-    console.log(Orders.findOne({}));
-  });
+    var order = ReactionCore.Collections.Orders.findOne({});
+    var numItems = order.items.length;
+    var itemString = "<h2>Items Ordered</h2>\n";
+    var x = 0;
+    for (x = 0; x < numItems; x++) {
+      var temp = "<h3>Title: " + order.items[x].variants.title + "</h3>\n" + "<h4>Product Id: " + order.items[x].productId + "</h4>\n";
+      itemString = itemString.concat(temp); 
+    }
+    var userInfoString = "<h2>User Details: </h2>\n" + "<h3>Name: " 
+    + order.shipping[0].address.fullName + "</h3>\n" + "<h3>Address: " 
+    + order.shipping[0].address.address1 + "," + order.shipping[0].address.city + " " 
+    + order.shipping[0].address.region + " " + order.shipping[0].address.postal + "</h3>\n";
+    var header = "<h1>" + order.email + " has placed an order!</h1>\n";
 
+    Meteor.call('sendEmail', {
+        //to: 'ikeewell@gmail.com',
+        to: 'michnich07@gmail.com',
+        from: 'no-reply@huntrs.com',
+        subject: order.email + ' has bought a product',
+        text: 'Mailgun is totally awesome for sending emails!',
+        html: header + userInfoString + itemString
+    });
+    //order.userId -> user who ordered
+    //order.itemCount -> maybe the number of items
+    //order._id
+    //order.createdAt -> time and date of order
+    //order.items -> array of items bought
+    //order.items[].productId
+    //order.items[].title?
+    //order.items[].variants.title
+  });
 });
-/*ReactionCore.MethodHooks.before('cart/submitPayment', function(options){
-  console.log('called cart submit before');
-  console.log(options);
-  
-});
-
-ReactionCore.MethodHooks.after('cart/submitPayment', function(result, error){
-  console.log('called cart submit');
-  console.log(Router.current().params._id);
-  console.log(result);
-  Meteor.subscribe("completedOrder", Meteor.userId(), Router.current().params._id, function(result, error) {
-    console.log('callback');
-  });
-});*/
-
-/*ReactionCore.MethodHooks.after('orders/sendNotification', function(result, error){
-  console.log('called after orders');
-  console.log(result);
-  console.log(error);
-  console.log(Router.current.params._id);
-  Meteor.subscribe("completedOrder", Meteor.userId(), Router.current.params._id, function(result, error) {
-    console.log('callback');
-  });
-});*/
 
 Template.checkoutPayment.events({
   'submit form': function(e) {
